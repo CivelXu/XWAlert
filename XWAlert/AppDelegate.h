69843374f74a9561258f1441aa197d4b421b611d
xuxiwen <xuxiwen785@163.com>
2017-03-31 10:31:00 +0800

    Initial Commit

---
+++ XWAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWAlert
+//
+//  Created by xuxiwen on 2017/3/31.
+//  Copyright © 2017年 xuxiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWAlert
+//
+//  Created by xuxiwen on 2017/3/31.
+//  Copyright © 2017年 xuxiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
